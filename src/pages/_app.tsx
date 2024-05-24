c7d8207891705be7a9524f043c0b52890fcd5bf7
Aldi Nugraha <aldi.nugrahatk@gmail.com>
2024-05-24 23:10:12 +0700

    restore _app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,7 +5,6 @@ import { type AppType } from "next/app";
 import { api } from "~/utils/api";
 
 import "~/styles/globals.css";
-import Head from "next/head";
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
@@ -13,17 +12,6 @@ const MyApp: AppType<{ session: Session | null }> = ({
 }) => {
   return (
     <SessionProvider session={session}>
-      <Head>
-        <title>
-          Sistem Informasi Administrasi dan Pembangunan - Kabupaten Pesisir
-          Barat
-        </title>
-        <meta
-          name="description"
-          content="Sistem Informasi Administrasi dan Pembangunan Kabupaten Pesisir Barat menyediakan layanan informasi dan administrasi pembangunan yang efisien dan transparan untuk masyarakat dan pemerintah daerah."
-        />
-        <link rel="icon" href="/logo.png" />
-      </Head>
       <Component {...pageProps} />
     </SessionProvider>
   );
