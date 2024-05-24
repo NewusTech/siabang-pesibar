36ed3a616ac0d7ae87305c3db1e316e1d2b627f1
Aldi Nugraha <aldi.nugrahatk@gmail.com>
2024-05-24 21:22:57 +0700

    revisi logo dan title

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -11,30 +11,15 @@ export const metadata: Metadata = {
 export default function AuthenticationPage() {
   return (
     <>
-      <div className="container h-[800px] flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
+      <div className="container flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
         <div className="relative">
           <div className="w-full h-screen hidden md:block">
             <Image src="/bg2.jpeg" alt="background" width={1000} height={2000} className="w-full h-full object-cover"/>
           </div>
           <div className="md:absolute hidden md:block inset-0 w-full h-screen bg-zinc-900 opacity-50 z-10"></div>
-          <div className="md:absolute inset-0 p-5 md:p-10 z-20">
-            <div className="flex md:text-white">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                className="mr-2 h-6 w-6"
-              >
-                <path d="M15 6v12a3 3 0 1 0 3-3H6a3 3 0 1 0 3 3V6a3 3 0 1 0-3 3h12a3 3 0 1 0-3-3"/>
-              </svg>
-              Siabang
-            </div>
-            <div className="items-center flex justify-center mt-2 md:mt-[200px] mb-[10px]">
-              <img className="md:h-[300px] h-32 items-center m-auto" alt='logo' src="/logo.png" style={{color: `white`}}/>
+          <div className="md:absolute inset-0 p-5 z-20">
+            <div className="items-center flex justify-center mt-2 md:mt-[30vh] mb-[10px]">
+              <img className="md:h-[45vh] h-32 items-center m-auto" alt='logo' src="/logo.png" style={{color: `white`}}/>
             </div>
             <div className="relative z-20 mt-auto md:text-white flex justify-center">
               <blockquote className="space-y-2">
